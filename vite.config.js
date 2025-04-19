b98b866e51909b2a864165064810e4bb53287c93
my.alchemy.star <my.alchemy.star@gmail.com>
2025-04-19 03:44:42 +0000

    new

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/https://github.com/nadsbalyamin/Educational-Minigames.git/',
+  base: '/Educational-Minigames/',
   plugins: [react()],
 })
